43022fb61871e419540bdde2b2c78fb9cc4651d3
overbejt <overbejt@miamioh.edu>
2019-11-27 20:56:41 -0500

    It connects to the database finally

--- setup/fillDb.py
+++ setup/fillDb.py
@@ -4,3 +4,26 @@ import sys
 
 print('hell0')
 
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',
+                       cursorclass=pymysql.cursors.DictCursor)
+try:
+    with conn.cursor() as cursor:
+        # Create a new record
+        sql = "select * from overbejt.gene"
+        cursor.execute(sql)
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    # conn.commit()
+
+    # with conn.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+    #     cursor.execute(sql, ('webmaster@python.org',))
+    #     result = cursor.fetchone()
+    #     print(result)
+finally:
+    conn.close()
+
