fdac82c95b15940e82ad52f40ee08bbf2cad6893
overbejt <overbejt@miamioh.edu>
2019-12-04 19:21:00 -0500

    asdf

--- gt82.py
+++ gt82.py
@@ -72,7 +72,7 @@ print('<div class="col-lg-8 col-md-8 col-sm-12 pt-5">')
 print('<div class="row"><!-- header row -->')
 print('<h1>Genes and Transcripts Annotated in the 82 Release Only</h1>')
 print('<a href="#genes" name="top"><h4>Genes</h4></a>')  # Internal Link
-print('<h4>Transcripts<a href="#transcripts"></a></h4>')  # Internal Link
+print('<br/><a href="#transcripts"><h4>Transcripts</h4></a>')  # Internal Link
 print('</div><!-- end of the header row -->')
 print('<div class="row pt-5"><!-- Gene table row -->')
 print('<a name="genes"><h2>Genes</h2></a>')
@@ -104,7 +104,7 @@ print('</table>')
 print('</div><!-- end of the Gene Category 1 table row -->')
 print('<div class="row pt-5"><!-- Transcripts table row -->')
 print('<a href="#top"><h4>Back to top</h4></a>')
-print('<a name="transcripts"><h2>Transcripts</h2></a>')
+print('<br/><a name="transcripts"><h2>Transcripts</h2></a>')
 print('<table class="table table-striped">')
 print('<thead class="bg-danger">')
 print('<tr>')
